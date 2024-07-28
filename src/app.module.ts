9be884625f6d8cca360bb3b0af7b42a5aee30095
benefrihw <gjtja7@naver.com>
2024-07-29 01:19:04 +0900

    add: entity 생성 #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SchedulerModule } from './scheduler/scheduler.module';
+import { SchedulerService } from './scheduler/scheduler.service';
 
 @Module({
   imports: [SchedulerModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SchedulerService],
 })
 export class AppModule {}
