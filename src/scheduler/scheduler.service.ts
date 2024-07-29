33bc25b5b73ca38df380b5e81e4dd12b37cbdd67
benefrihw <gjtja7@naver.com>
2024-07-29 16:52:57 +0900

    feat: 알림 생성 및 NextDate 업데이트 테스트 완료 #2

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -21,16 +21,16 @@ export class SchedulerService {
     private subscriptionHistoriesRepository: Repository<SubscriptionHistories>,
   ) {}
 
-  @Cron('00 34 15 * * *')
+  @Cron('00 47 16 * * *')
   async handleCron() {
-    this.logger.debug('알림 테스트');
+    this.logger.debug('알림 시작!');
 
     // 결제 이력 가져오기
     const subscriptionHistories =
       await this.subscriptionHistoriesRepository.find({
         relations: ['userSubscription', 'userSubscription.user'],
       });
-    console.log(subscriptionHistories);
+    console.log('결제이력', subscriptionHistories);
 
     // today 설정
     const today = new Date();
@@ -38,9 +38,9 @@ export class SchedulerService {
     today.setHours(0, 0, 0, 0);
     console.log('today', today);
     // 결제정보 순회하면서 다음 결제일 가져오기
-    for (const paymentHistory of subscriptionHistories) {
+    for (const subscriptionHistory of subscriptionHistories) {
       // 결제일 설정(일단, 결제 시작일)
-      const payDate = paymentHistory.nextDate;
+      const payDate = subscriptionHistory.nextDate;
       console.log('payDate', payDate);
       // 알람일 설정(결제일 -1)
       const notifyingDate = new Date(payDate);
@@ -49,77 +49,34 @@ export class SchedulerService {
       console.log('notifyingDate', notifyingDate);
 
       // today와 notifyingDate 비교
-      if (notifyingDate.getDate() == today.getDate()) {
-        const message = '결제일 1일 전입니다.';
+      if (notifyingDate.getTime() === today.getTime()) {
+        const userNickname = subscriptionHistory.userSubscription.user.nickname;
+        const message = `${userNickname}님 결제일 1일 전입니다.`;
         console.log(message);
 
-        // notification 저장
+        // notification DB에 저장
         const newNotification = this.notificationRepository.create({
-          userId: paymentHistory.userSubscription.userId,
-          userSubscriptionId: paymentHistory.userSubscriptionId,
+          userId: subscriptionHistory.userSubscription.userId,
+          userSubscriptionId: subscriptionHistory.userSubscriptionId,
           title: message,
           isRead: false,
           createdAt: new Date(),
-          readedAt: new Date(),
+          readedAt: new Date(), // Default를 null로 지정필요
         });
-        console.log(newNotification);
+        console.log('알림 발생', newNotification);
         await this.notificationRepository.save(newNotification);
+
+        // subscriptionHistory의 NextDate 업데이트
+        const nextPayDate = new Date(payDate);
+        nextPayDate.setMonth(nextPayDate.getMonth() + 1);
+        await this.subscriptionHistoriesRepository.update(subscriptionHistory, {
+          nextDate: nextPayDate,
+        });
+      } else {
+        // 알림 미생성 테스트를 위해 추가
+        const userNickname = subscriptionHistory.userSubscription.user.nickname;
+        console.log('알림 미발생', `${userNickname} 알림받을 날짜가 아닙니다`);
       }
     }
   }
-
-  //   @Cron('00 13 18 * * *')
-  //   async handleCron() {
-  //     this.logger.debug('알림 테스트');
-
-  //     // 구독 정보 가져오기
-  //     const userSubscription = await this.userSubscriptionsRepository.find({
-  //       relations: ['user'],
-  //     });
-  //     console.log(userSubscription);
-
-  //     // today 설정
-  //     const today = new Date();
-  //     // 날짜만 필요해서 시간은 초기화
-  //     today.setHours(0, 0, 0, 0);
-  //     console.log('today', today);
-  //     // 유저의 구독정보를 돌면서 결제 시작일 가져오기
-  //     userSubscription.forEach(async (userSubscription) => {
-  //       // 결제일 설정(일단, 결제 시작일)
-  //       const payDate = userSubscription.startedDate;
-  //       console.log('payDate', payDate);
-  //       // 알람일 설정(결제일 -1)
-  //       const notifyingDate = new Date(payDate);
-  //       notifyingDate.setDate(notifyingDate.getDate() - 1);
-  //       notifyingDate.setHours(0, 0, 0, 0);
-  //       console.log('notifyingDate', notifyingDate);
-
-  //       // today와 notifyingDate 비교
-  //       if (notifyingDate.getDate() == today.getDate()) {
-  //         const message = '결제일 1일 전입니다.';
-  //         console.log(message);
-
-  //         // notification 저장
-  //         const newNotification = this.notificationRepository.create({
-  //           userSubscription,
-  //           userId: userSubscription.userId,
-  //           userSubscriptionId: userSubscription.id,
-  //           title: message,
-  //           isRead: false,
-  //           createdAt: new Date(),
-  //         });
-  //         await this.notificationRepository.save(newNotification);
-
-  //         // Historied 저장
-  //         const nextPayDate = new Date(payDate);
-  //         nextPayDate.setMonth(nextPayDate.getMonth() + 1);
-  //         const updateHistory = this.subscriptionHistoriesRepository.create({
-  //           userSubscription,
-  //           userSubscriptionId: userSubscription.id,
-  //           nextDate: nextPayDate,
-  //         });
-  //         await this.subscriptionHistoriesRepository.save(updateHistory);
-  //       }
-  //     });
-  //   }
 }
