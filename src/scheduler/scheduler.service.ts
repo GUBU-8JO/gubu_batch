56f221018b5edd84e29d2e787a779cb38d0ce4c8
gil-hyeon <lgh5498@naver.com>
2024-08-21 21:32:21 +0900

    feat: 필요없는 주석 삭제

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
-import { IsNull, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Notification } from './entities/notification.entity';
 import { SubscriptionHistory } from './entities/subscription-histories.entity';
@@ -13,90 +13,17 @@ export class SchedulerService {
   private readonly logger = new Logger(SchedulerService.name);
   constructor(
     @InjectRepository(Notification)
-    private notificationRepository: Repository<Notification>,
+    //private notificationRepository: Repository<Notification>,
     @InjectRepository(SubscriptionHistory)
-    private subscriptionHistoriesRepository: Repository<SubscriptionHistory>,
-    private readonly redisService: RedisService,
+    private readonly //private subscriptionHistoriesRepository: Repository<SubscriptionHistory>,
+    redisService: RedisService,
     private readonly slackService: SlackService,
     private readonly reviewRepository: ReviewRepository,
     private readonly platformRepository: PlatformRepository,
   ) {}
 
-  /** 알림 생성 스케쥴링 */
-  // @Cron('* 10 * * * *')
-  // async createNotification() {
-  //   this.logger.debug('알림 시작!');
-
-  //   try {
-  //     // 다음 결제일이 내일인 결제 이력 가져오기
-  //     const tomorrow = new Date();
-  //     tomorrow.setHours(0, 0, 0, 0); // 시간을 00:00:00으로 설정
-  //     tomorrow.setDate(tomorrow.getDate() + 1);
-
-  //     const subscriptionHistories =
-  //       await this.subscriptionHistoriesRepository.find({
-  //         where: {
-  //           nextPayAt: tomorrow,
-  //           stopRequestAt: IsNull(),
-  //         },
-  //         relations: [
-  //           'userSubscription',
-  //           'userSubscription.user',
-  //           'userSubscription.platform',
-  //         ],
-  //       });
-  //     // console.log('내일인 결제이력', subscriptionHistories);
-
-  //     if (subscriptionHistories.length <= 0) {
-  //       this.logger.debug('알림을 생성할 결제 이력이 없습니다.');
-  //     } else {
-  //       const notifications = subscriptionHistories.map(
-  //         (subscriptionHistory) => {
-  //           const userNickname =
-  //             subscriptionHistory.userSubscription.user.nickname;
-  //           const platformTitle =
-  //             subscriptionHistory.userSubscription.platform.title;
-  //           const message = `${userNickname}님 ${platformTitle} 결제일 1일 전입니다.`;
-
-  //           return this.notificationRepository.create({
-  //             userId: subscriptionHistory.userSubscription.userId,
-  //             userSubscriptionId: subscriptionHistory.userSubscriptionId,
-  //             title: message,
-  //             isRead: false,
-  //             createdAt: new Date(),
-  //             readedAt: null,
-  //           });
-  //         },
-  //       );
-  //       console.log(notifications, 'notifi');
-  //       // return this.subscriptionHistoriesRepository.save(notifications);
-  //       // await this.notificationRepository.save(notifications);
-  //       // console.log('알림 발생', notifications);
-
-  //       // const updatePromises = subscriptionHistories.map(
-  //       //   (subscriptionHistory) => {
-  //       //     const nextPayDate = new Date(subscriptionHistory.nextPayAt);
-  //       //     const period = subscriptionHistory.userSubscription.period;
-  //       //     nextPayDate.setMonth(nextPayDate.getMonth() + period);
-
-  //       //     return this.subscriptionHistoriesRepository.update(
-  //       //       subscriptionHistory.id,
-  //       //       {
-  //       //         nextPayAt: nextPayDate,
-  //       //       },
-  //       //     );
-  //       //   },
-  //       // );
-  //       // await Promise.all(updatePromises);
-  //     }
-  //   } catch (err) {
-  //     this.logger.error('알림 생성 실패', err.stack);
-  //     await this.slackService.sendMessage('알림 생성에 실패했습니다.');
-  //   }
-  // }
-
   /** 평점 계산 스케쥴링 */
-  @Cron('10 * * * * *')
+  @Cron('* 0 */1 * * *')
   async ratingCalculation() {
     this.logger.debug('평점 계산 시작!');
 
